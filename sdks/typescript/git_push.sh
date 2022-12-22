2e10488e79d974c061c8b0d260cf99f472a74c0e
dphuang2 <dylan.p.huang@gmail.com>
2022-12-22 11:30:20 -0800

    regenerate SDKs

--- sdks/typescript/git_push.sh
+++ sdks/typescript/git_push.sh
@@ -14,12 +14,12 @@ if [ "$git_host" = "" ]; then
 fi
 
 if [ "$git_user_id" = "" ]; then
-    git_user_id="konfig-dev"
+    git_user_id="passiv"
     echo "[INFO] No command line input provided. Set \$git_user_id to $git_user_id"
 fi
 
 if [ "$git_repo_id" = "" ]; then
-    git_repo_id="snaptrade-typescript-sdk"
+    git_repo_id="snaptrade-sdks/tree/main/sdks/typescript"
     echo "[INFO] No command line input provided. Set \$git_repo_id to $git_repo_id"
 fi
 
