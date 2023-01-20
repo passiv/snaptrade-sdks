4331636edad24628c54d9d819bdfb97f16f2d59b
dphuang2 <dylan.p.huang@gmail.com>
2023-01-20 14:14:01 -0800

    use readmeSnippet for typescript

--- snippets/typescript.ts
+++ snippets/typescript.ts
@@ -1,41 +1,3 @@
-## snaptrade-typescript-sdk@2.0.2
-
-This generator creates TypeScript/JavaScript client that utilizes [axios](https://github.com/axios/axios). The generated Node module can be used in the following environments:
-
-Environment
-* Node.js
-* Webpack
-* Browserify
-
-Language level
-* ES5 - you must have a Promises/A+ library installed
-* ES6
-
-Module system
-* CommonJS
-* ES6 module system
-
-It can be used in both TypeScript and JavaScript. In TypeScript, the definition should be automatically resolved via `package.json`. ([Reference](http://www.typescriptlang.org/docs/handbook/typings-for-npm-packages.html))
-
-### Building
-
-To build and compile the typescript sources to javascript use:
-```
-npm install
-npm run build
-```
-
-### Consuming
-
-navigate to the folder of your consuming project and run the following command.
-
-```
-npm install snaptrade-typescript-sdk@2.0.2 --save
-```
-
-### Getting Started
-
-```typescript
 const {
   Configuration,
   APIStatusApi,
@@ -113,5 +75,3 @@ function getUserId() {
 }
 
 main();
-
-```
