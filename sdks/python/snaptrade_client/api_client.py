50ee92bf203c5b8d909793c16d2f9a0a374e9796
dphuang2 <dylan.p.huang@gmail.com>
2023-02-28 12:28:28 -0800

    major bump / regenerate Python SDK

--- sdks/python/snaptrade_client/api_client.py
+++ sdks/python/snaptrade_client/api_client.py
@@ -1071,7 +1071,7 @@ class ApiClient:
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'Konfig/4.0.0/python'
+        self.user_agent = 'Konfig/5.0.0/python'
 
     def __enter__(self):
         return self
