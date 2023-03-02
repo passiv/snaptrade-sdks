972e96cd88eb33f614833b9260ccbe50ae4c8ab7
dphuang2 <dylan.p.huang@gmail.com>
2023-03-01 20:59:52 -0800

    update

--- sdks/typescript/common.ts
+++ sdks/typescript/common.ts
@@ -14,116 +14,88 @@
 
 import { Configuration } from "./configuration";
 import { RequiredError, RequestArgs } from "./base";
-import { AxiosInstance, AxiosResponse } from "axios";
+import { AxiosInstance, AxiosResponse } from 'axios';
 import { requestAfterHook } from "./requestAfterHook";
 
 /**
  *
  * @export
  */
-export const DUMMY_BASE_URL = "https://example.com";
+export const DUMMY_BASE_URL = 'https://example.com'
 
 /**
  *
  * @throws {RequiredError}
  * @export
  */
-export const assertParamExists = function (
-  functionName: string,
-  paramName: string,
-  paramValue: unknown
-) {
-  if (paramValue === null || paramValue === undefined) {
-    throw new RequiredError(
-      paramName,
-      `Required parameter ${paramName} was null or undefined when calling ${functionName}.`
-    );
-  }
-};
-
-/**
- *
- * @export
- */
-export const setApiKeyToObject = async function (
-  object: any,
-  keyParamName: string,
-  configuration?: Configuration
-) {
-  if (configuration && configuration.apiKey) {
-    if (typeof configuration.apiKey === "function")
-      object[keyParamName] = await configuration.apiKey(keyParamName);
-    if (typeof configuration.apiKey === "string")
-      object[keyParamName] = configuration.apiKey;
-    if (typeof configuration.apiKey === "object") {
-      if (keyParamName in configuration.apiKey)
-        object[keyParamName] = configuration.apiKey[keyParamName];
-    } else
-      throw Error(
-        `Unexpected type ${typeof configuration.apiKey} for Configuration.apiKey`
-      );
-  }
-};
-
-/**
- *
- * @export
- */
-export const setBasicAuthToObject = function (
-  object: any,
-  configuration?: Configuration
-) {
-  if (configuration && (configuration.username || configuration.password)) {
-    object["auth"] = {
-      username: configuration.username,
-      password: configuration.password,
-    };
-  }
-};
-
-/**
- *
- * @export
- */
-export const setBearerAuthToObject = async function (
-  object: any,
-  configuration?: Configuration
-) {
-  if (configuration && configuration.accessToken) {
-    const accessToken =
-      typeof configuration.accessToken === "function"
-        ? await configuration.accessToken()
-        : await configuration.accessToken;
-    object["Authorization"] = "Bearer " + accessToken;
-  }
-};
-
-function setFlattenedQueryParams(
-  urlSearchParams: URLSearchParams,
-  parameter: any,
-  key: string = ""
-): void {
-  if (typeof parameter === "object") {
-    if (Array.isArray(parameter)) {
-      (parameter as any[]).forEach((item) =>
-        setFlattenedQueryParams(urlSearchParams, item, key)
-      );
-    } else {
-      Object.keys(parameter).forEach((currentKey) =>
-        setFlattenedQueryParams(
-          urlSearchParams,
-          parameter[currentKey],
-          `${key}${key !== "" ? "." : ""}${currentKey}`
-        )
-      );
+export const assertParamExists = function (functionName: string, paramName: string, paramValue: unknown) {
+    if (paramValue === null || paramValue === undefined) {
+        throw new RequiredError(paramName, `Required parameter ${paramName} was null or undefined when calling ${functionName}.`);
     }
-  } else {
-    if (urlSearchParams.has(key)) {
-      urlSearchParams.append(key, parameter);
-    } else {
-      urlSearchParams.set(key, parameter);
+}
+
+/**
+ *
+ * @export
+ */
+export const setApiKeyToObject = async function (object: any, keyParamName: string, configuration?: Configuration) {
+    if (configuration && configuration.apiKey) {
+      if (typeof configuration.apiKey === "function")
+        object[keyParamName] = await configuration.apiKey(keyParamName);
+      if (typeof configuration.apiKey === "string")
+        object[keyParamName] = configuration.apiKey;
+      if (typeof configuration.apiKey === "object") {
+        if (keyParamName in configuration.apiKey)
+          object[keyParamName] = configuration.apiKey[keyParamName];
+      } else
+        throw Error(
+          `Unexpected type ${typeof configuration.apiKey} for Configuration.apiKey`
+        );
     }
-  }
+}
+
+/**
+ *
+ * @export
+ */
+export const setBasicAuthToObject = function (object: any, configuration?: Configuration) {
+    if (configuration && (configuration.username || configuration.password)) {
+        object["auth"] = { username: configuration.username, password: configuration.password };
+    }
+}
+
+/**
+ *
+ * @export
+ */
+export const setBearerAuthToObject = async function (object: any, configuration?: Configuration) {
+    if (configuration && configuration.accessToken) {
+        const accessToken = typeof configuration.accessToken === 'function'
+            ? await configuration.accessToken()
+            : await configuration.accessToken;
+        object["Authorization"] = "Bearer " + accessToken;
+    }
+}
+
+function setFlattenedQueryParams(urlSearchParams: URLSearchParams, parameter: any, key: string = ""): void {
+    if (typeof parameter === "object") {
+        if (Array.isArray(parameter)) {
+            (parameter as any[]).forEach(item => setFlattenedQueryParams(urlSearchParams, item, key));
+        } 
+        else {
+            Object.keys(parameter).forEach(currentKey => 
+                setFlattenedQueryParams(urlSearchParams, parameter[currentKey], `${key}${key !== '' ? '.' : ''}${currentKey}`)
+            );
+        }
+    } 
+    else {
+        if (urlSearchParams.has(key)) {
+            urlSearchParams.append(key, parameter);
+        } 
+        else {
+            urlSearchParams.set(key, parameter);
+        }
+    }
 }
 
 /**
@@ -131,54 +103,41 @@ function setFlattenedQueryParams(
  * @export
  */
 export const setSearchParams = function (url: URL, ...objects: any[]) {
-  const searchParams = new URLSearchParams(url.search);
-  setFlattenedQueryParams(searchParams, objects);
-  url.search = searchParams.toString();
-};
+    const searchParams = new URLSearchParams(url.search);
+    setFlattenedQueryParams(searchParams, objects);
+    url.search = searchParams.toString();
+}
 
 /**
  *
  * @export
  */
-export const serializeDataIfNeeded = function (
-  value: any,
-  requestOptions: any,
-  configuration?: Configuration
-) {
-  const nonString = typeof value !== "string";
-  const needsSerialization =
-    nonString && configuration && configuration.isJsonMime
-      ? configuration.isJsonMime(requestOptions.headers["Content-Type"])
-      : nonString;
-  return needsSerialization
-    ? JSON.stringify(value !== undefined ? value : {})
-    : value || "";
-};
+export const serializeDataIfNeeded = function (value: any, requestOptions: any, configuration?: Configuration) {
+    const nonString = typeof value !== 'string';
+    const needsSerialization = nonString && configuration && configuration.isJsonMime
+        ? configuration.isJsonMime(requestOptions.headers['Content-Type'])
+        : nonString;
+    return needsSerialization
+        ? JSON.stringify(value !== undefined ? value : {})
+        : (value || "");
+}
 
 /**
  *
  * @export
  */
 export const toPathString = function (url: URL) {
-  return url.pathname + url.search + url.hash;
-};
+    return url.pathname + url.search + url.hash
+}
 
 /**
  *
  * @export
  */
-export const createRequestFunction = function (
-  axiosArgs: RequestArgs,
-  globalAxios: AxiosInstance,
-  BASE_PATH: string,
-  configuration?: Configuration
-) {
-  return <T = unknown, R = AxiosResponse<T>>(
-    axios: AxiosInstance = globalAxios,
-    basePath: string = BASE_PATH
-  ) => {
-    const url = (configuration?.basePath || basePath) + axiosArgs.url;
-    requestAfterHook({ axiosArgs, basePath, url, configuration });
-    return axios.request<T, R>({ ...axiosArgs.options, url });
-  };
-};
+export const createRequestFunction = function (axiosArgs: RequestArgs, globalAxios: AxiosInstance, BASE_PATH: string, configuration?: Configuration) {
+    return <T = unknown, R = AxiosResponse<T>>(axios: AxiosInstance = globalAxios, basePath: string = BASE_PATH) => {
+        const url = (configuration?.basePath || basePath) + axiosArgs.url
+        requestAfterHook({axiosArgs, basePath, url, configuration})
+        return axios.request<T, R>({...axiosArgs.options, url});
+    };
+}
