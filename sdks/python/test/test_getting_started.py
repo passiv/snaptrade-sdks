7bd107f4cda8948f865184e292e4c7607a9f4d16
dphuang2 <dylan.p.huang@gmail.com>
2023-04-11 14:42:16 -0700

    save test

--- sdks/python/test/test_getting_started.py
+++ sdks/python/test/test_getting_started.py
@@ -9,6 +9,7 @@
 """
 
 
+import datetime
 import os
 import unittest
 import uuid
@@ -94,6 +95,48 @@ class TestGettingStarted(unittest.TestCase):
         account_id = accounts.body[0]["id"]
         holdings = snaptrade.account_information.get_user_holdings(query_params={"userId": user_id, "userSecret": user_secret}, path_params={"accountId": account_id})
         pprint(holdings)
+
+    def test_get_activities(self):
+        snaptrade = SnapTrade(
+            consumer_key=os.environ['SNAPTRADE_CONSUMER_KEY'],
+            client_id=os.environ["SNAPTRADE_CLIENT_ID"]
+        )
+        user_id = os.environ["SNAPTRADE_TEST_USER_ID"]
+        user_secret = os.environ["SNAPTRADE_TEST_USER_SECRET"]
+        # instantiate two variables, one date that is 1 year ago and another that is today
+        from_date = datetime.datetime.now() - datetime.timedelta(days=365)
+        to_date = datetime.datetime.now()
+        response = snaptrade.transactions_and_reporting.get_activities(query_params={
+            "userId": user_id,
+            "userSecret": user_secret,
+            "startDate": from_date,
+            "endDate": to_date
+        })
+        pprint(response)
+        self.assertIsNotNone(response)
+        # instantiate two variables of type "date", one date that is 1 year ago and another that is today
+        from_date = datetime.date.today() - datetime.timedelta(days=365)
+        to_date = datetime.date.today()
+        response = snaptrade.transactions_and_reporting.get_activities(query_params={
+            "userId": user_id,
+            "userSecret": user_secret,
+            "startDate": from_date,
+            "endDate": to_date
+        })
+        pprint(response)
+        self.assertIsNotNone(response)
+        # instantiate two variables of type "string" in yyyy-mm-dd format, one date that is 1 year ago and another that is today
+        from_date = (datetime.date.today() - datetime.timedelta(days=365)).strftime("%Y-%m-%d")
+        to_date = datetime.date.today().strftime("%Y-%m-%d")
+        response = snaptrade.transactions_and_reporting.get_activities(query_params={
+            "userId": user_id,
+            "userSecret": user_secret,
+            "startDate": from_date,
+            "endDate": to_date
+        })
+        pprint(response)
+        # assert that response is not None
+        self.assertIsNotNone(response)
 
     @unittest.skip(reason="getting 500 internal server error")
     def test_get_options_chain(self):
