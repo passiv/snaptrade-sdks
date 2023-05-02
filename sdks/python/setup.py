d7ce0f06227ee3707c796a973906c205ab1480cc
dphuang2 <dylan.p.huang@gmail.com>
2023-05-02 11:57:28 -0700

    minor bump + regenerate Python SDK

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "snaptrade-python-sdk"
-VERSION = "10.8.0"
+VERSION = "10.9.0"
 # To install the library, run the following
 #
 # python setup.py install
--- sdks/python/snaptrade_client/api_client.py
+++ sdks/python/snaptrade_client/api_client.py
@@ -1070,7 +1070,7 @@ class ApiClient:
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'Konfig/10.8.0/python'
+        self.user_agent = 'Konfig/10.9.0/python'
 
     def __enter__(self):
         return self
