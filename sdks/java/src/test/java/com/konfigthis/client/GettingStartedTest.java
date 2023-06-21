9e5e150d73ccf12cb7e7e384b2e90efe3cc001b1
dphuang2 <dylan.p.huang@gmail.com>
2023-06-20 17:01:07 -0700

    rewrite GettingStartedTest.java for new interface

--- sdks/java/src/test/java/com/konfigthis/client/GettingStartedTest.java
+++ sdks/java/src/test/java/com/konfigthis/client/GettingStartedTest.java
@@ -10,11 +10,11 @@ import java.util.UUID;
 
 public class GettingStartedTest {
 
-    // 1) Initialize default client with clientID and consumerKey
-    static {
-        Configuration.setDefaultClientId(System.getenv("SNAPTRADE_CLIENT_ID"));
-        Configuration.setDefaultConsumerKey(System.getenv("SNAPTRADE_CONSUMER_KEY"));
-    }
+        // 1) Initialize default client with clientID and consumerKey
+        static {
+                Configuration.setDefaultClientId(System.getenv("SNAPTRADE_CLIENT_ID"));
+                Configuration.setDefaultConsumerKey(System.getenv("SNAPTRADE_CONSUMER_KEY"));
+        }
 
     @Test
     public void gettingStartedTest() throws ApiException {
@@ -22,7 +22,7 @@ public class GettingStartedTest {
 
         // 2) Check that the client is able to make a request to the API server
         ApiStatusApi apiStatusApi = new ApiStatusApi(defaultClient);
-        Status status = apiStatusApi.check();
+        Status status = apiStatusApi.check().execute();
         System.out.printf("SnapTrade is online: %s\n", status.getOnline());
 
         // 3) Create a new user on SnapTrade
@@ -31,16 +31,18 @@ public class GettingStartedTest {
         // The userId should be provided by you and refer to permanent value such as a
         // database row ID
         UUID userId = UUID.randomUUID();
-        UserIDandSecret userIDandSecret = authenticationApi.registerSnapTradeUser(new SnapTradeRegisterUserRequestBody().userId(userId.toString()));
+        UserIDandSecret userIDandSecret = authenticationApi.registerSnapTradeUser().userId(userId.toString()).execute();
+
+        new SnapTradeRegisterUserRequestBody().userId(userId.toString()));
         // Note: A user secret is only generated once. It's required to access resources
         // for certain endpoints
         System.out.printf("userID: %s, userSecret: %s\n", userIDandSecret.getUserId(), userIDandSecret.getUserSecret());
 
         // 4) Get a redirect URI. Users will need this to connect their brokerage to the
         // SnapTrade server
-        AuthenticationLoginSnapTradeUser200Response response = authenticationApi.loginSnapTradeUser(userIDandSecret.getUserId(),
-                userIDandSecret.getUserSecret(), new SnapTradeLoginUserRequestBody());
-        System.out.println(response.getLoginRedirectURI().getRedirectURI());
+        LoginRedirectURI response = (LoginRedirectURI) authenticationApi.loginSnapTradeUser(userIDandSecret.getUserId(),
+                userIDandSecret.getUserSecret()).execute();
+        System.out.println(response.getRedirectURI());
 
         // 5) Make a portfolio group and query
         PortfolioManagementApi portfolioManagementApi = new PortfolioManagementApi(defaultClient);
@@ -48,34 +50,28 @@ public class GettingStartedTest {
         portfolioGroupPostBody.put("id", UUID.randomUUID().toString());
         portfolioGroupPostBody.put("name", "MyPortfolio");
         List<PortfolioGroup> portfolioGroupsFromPost = portfolioManagementApi.create(
-                userIDandSecret.getUserId(), userIDandSecret.getUserSecret(), portfolioGroupPostBody);
+                userIDandSecret.getUserId(), userIDandSecret.getUserSecret()).id(UUID.randomUUID()).name("MyPortfolio").execute();
         System.out.println(portfolioGroupsFromPost);
-        List<PortfolioGroup> portfolioGroups = portfolioManagementApi.callList(userIDandSecret.getUserId(),
-                userIDandSecret.getUserSecret());
+        List<PortfolioGroup> portfolioGroups = portfolioManagementApi.list(userIDandSecret.getUserId(),
+                userIDandSecret.getUserSecret()).execute();
         System.out.println(portfolioGroups);
 
-        // 6) Accept the disclaimer
-        ApiDisclaimerApi apiDisclaimerApi = new ApiDisclaimerApi(defaultClient);
-        SnapTradeAPIDisclaimerAcceptStatus snapTradeAPIDisclaimerAcceptStatus = apiDisclaimerApi
-                .accept(userIDandSecret.getUserId(), userIDandSecret.getUserSecret(), new APIDisclaimerAcceptRequest().accepted(true));
-        System.out.println(snapTradeAPIDisclaimerAcceptStatus);
-
-        AccountInformationApi accountInformationApi = new AccountInformationApi(defaultClient);
-        ReferenceDataApi referenceDataApi = new ReferenceDataApi(defaultClient);
 
         // 7) Query holdings and available brokerages
+        AccountInformationApi accountInformationApi = new AccountInformationApi(defaultClient);
+        ReferenceDataApi referenceDataApi = new ReferenceDataApi(defaultClient);
         List<AccountHoldings> holdings = accountInformationApi.getAllUserHoldings(userIDandSecret.getUserId(),
-                userIDandSecret.getUserSecret(), null);
+                userIDandSecret.getUserSecret()).execute();
         System.out.println(holdings);
         List<Account> accounts = accountInformationApi.listUserAccounts(userIDandSecret.getUserId(),
-                userIDandSecret.getUserSecret());
+                userIDandSecret.getUserSecret()).execute();
         System.out.println(accounts);
-        List<Brokerage> brokerages = referenceDataApi.listAllBrokerages();
+        List<Brokerage> brokerages = referenceDataApi.listAllBrokerages().execute();
         System.out.println(brokerages);
 
         // 8) Deleting a user
         DeleteUserResponse deleteUserResponse = authenticationApi
-                .deleteSnapTradeUser(userIDandSecret.getUserId());
+                .deleteSnapTradeUser(userIDandSecret.getUserId()).execute();
         System.out.println(deleteUserResponse);
     }
 
