c656245c457f4e4b9ab1aec43c5ffb90a36fdce1
dphuang2 <dylan.p.huang@gmail.com>
2023-08-02 13:14:49 -0700

    minor bump + regenerate SDKs

--- sdks/python/snaptrade_client/__init__.py
+++ sdks/python/snaptrade_client/__init__.py
@@ -12,7 +12,7 @@
     Created by: https://snaptrade.com/
 """
 
-__version__ = "10.25.0"
+__version__ = "10.26.0"
 
 # import ApiClient
 from snaptrade_client.api_client import ApiClient
--- sdks/typescript/README.md
+++ sdks/typescript/README.md
@@ -1,4 +1,4 @@
-# snaptrade-typescript-sdk@8.21.0
+# snaptrade-typescript-sdk@8.22.0
 
 Connect brokerage accounts to your app for live positions and trading
 ## Installing
