59838067594eec19855a77b1da00137802892459
konfig-publisher <publisher@konfigthis.com>
2023-09-06 22:16:03 -0700

    bump + generate

--- sdks/python/snaptrade_client/api_client.py
+++ sdks/python/snaptrade_client/api_client.py
@@ -1154,7 +1154,7 @@ class ApiClient:
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'Konfig/10.33.0/python'
+        self.user_agent = 'Konfig/10.33.1/python'
 
     def __enter__(self):
         return self
