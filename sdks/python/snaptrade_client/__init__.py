54b5aa5a997fa646418b91ccaa859f1d2ef932e3
konfig-publisher <publisher@konfigthis.com>
2024-04-12 13:53:35 -0700

    Version Bump SDKs

--- sdks/python/snaptrade_client/__init__.py
+++ sdks/python/snaptrade_client/__init__.py
@@ -12,7 +12,7 @@
     Created by: https://snaptrade.com/
 """
 
-__version__ = "11.0.11"
+__version__ = "11.0.12"
 
 # import ApiClient
 from snaptrade_client.api_client import ApiClient
--- sdks/typescript/configuration.ts
+++ sdks/typescript/configuration.ts
@@ -120,7 +120,7 @@ export class Configuration {
         this.accessToken = param.accessToken;
         this.basePath = param.basePath;
         this.baseOptions = param.baseOptions ?? {};
-        this.userAgent = param.userAgent === undefined ? "Konfig/9.0.13/typescript" : param.userAgent;
+        this.userAgent = param.userAgent === undefined ? "Konfig/9.0.14/typescript" : param.userAgent;
         this.formDataCtor = param.formDataCtor;
     }
 
