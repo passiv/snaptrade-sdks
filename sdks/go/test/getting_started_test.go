fdf0ae5223ba5e4234aac12b73568577c16bdd27
dphuang2 <dylan@konfigthis.com>
2024-08-01 17:35:23 -0700

    add clientState to go SDK

--- sdks/go/test/getting_started_test.go
+++ sdks/go/test/getting_started_test.go
@@ -25,18 +25,20 @@ func Test_snaptrade_GettingStarted(t *testing.T) {
 		assert.Equal(t, 200, httpRes.StatusCode)
 	})
 
-	// t.Run("Test GetUserAccountInformation", func(t *testing.T) {
-	// 	configuration := snaptrade.NewConfiguration()
-	// 	configuration.SetHost("http://127.0.0.1:4010")
-	// 	configuration.SetPartnerClientId("CLIENT_ID")
-	// 	client := snaptrade.NewAPIClient(configuration)
+	t.Run("Test GettingStarted", func(t *testing.T) {
+		configuration := snaptrade.NewConfiguration()
+		configuration.SetPartnerClientId("CLIENT_ID")
+		configuration.SetConsumerKey("CONSUMER_KEY")
+		client := snaptrade.NewAPIClient(configuration)
 
-	// 	request := client.AccountInformationApi.GetUserAccountDetails("userId", "userSecret", "accountId")
+		// Create a new user
 
-	// 	resp, httpRes, err := request.Execute()
+		request := client.AccountInformationApi.GetUserAccountDetails("userId", "userSecret", "accountId")
 
-	// 	require.Nil(t, err)
-	// 	require.NotNil(t, resp)
-	// 	assert.Equal(t, 200, httpRes.StatusCode)
-	// })
+		resp, httpRes, err := request.Execute()
+
+		require.Nil(t, err)
+		require.NotNil(t, resp)
+		assert.Equal(t, 200, httpRes.StatusCode)
+	})
 }
