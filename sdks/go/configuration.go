4517fe8ec94278443fb46f128d222fd05fed2580
konfig-publisher <publisher@konfigthis.com>
2024-09-09 20:02:41 +0000

    Version Bump SDKs

--- sdks/go/configuration.go
+++ sdks/go/configuration.go
@@ -105,7 +105,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "Konfig/1.0.14/go",
+		UserAgent:        "Konfig/1.0.15/go",
 		TokenUrl:   	  "",
 		Debug:            false,
         Context: context.Background(),
