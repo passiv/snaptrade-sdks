e433259d48f265305a3241fd98875db33048efe4
konfig-publisher <publisher@konfigthis.com>
2024-11-07 00:09:46 +0000

    Version Bump SDKs

--- sdks/typescript/configuration.ts
+++ sdks/typescript/configuration.ts
@@ -120,7 +120,7 @@ export class Configuration {
         this.accessToken = param.accessToken;
         this.basePath = param.basePath;
         this.baseOptions = param.baseOptions ?? {};
-        this.userAgent = param.userAgent === undefined ? "Konfig/9.0.52/typescript" : param.userAgent;
+        this.userAgent = param.userAgent === undefined ? "Konfig/9.0.53/typescript" : param.userAgent;
         this.formDataCtor = param.formDataCtor;
     }
 
