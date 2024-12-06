9a3bc30554cbfa8435dfce10b1721fbcc7a6ebf4
konfig-publisher <publisher@konfigthis.com>
2024-12-06 23:13:58 +0000

    Regenerate SDKs

--- sdks/ruby/lib/snaptrade/models/snap_trade_holdings_total_value.rb
+++ sdks/ruby/lib/snaptrade/models/snap_trade_holdings_total_value.rb
@@ -11,7 +11,7 @@ require 'date'
 require 'time'
 
 module SnapTrade
-  # This field is deprecated. To get the brokerage reported total market value of the account, please refer to `account.balance.total`. The total market value of the account. Note that this field is calculated based on the sum of the values of account positions and cash balances known to SnapTrade. It may not be accurate if the brokerage account has holdings that SnapTrade is not aware of. For example, if the brokerage account holds assets that SnapTrade does not support, the total value may be underreported. In certain cases, this value may also be double-counting cash-equivalent assets if those assets are represented as both cash and positions in the account.
+  # This field is deprecated. To get the brokerage reported total market value of the account, please refer to `account.balance.total`.  The total market value of the account. Note that this field is calculated based on the sum of the values of account positions and cash balances known to SnapTrade. It may not be accurate if the brokerage account has holdings that SnapTrade is not aware of. For example, if the brokerage account holds assets that SnapTrade does not support, the total value may be underreported. In certain cases, this value may also be double-counting cash-equivalent assets if those assets are represented as both cash and positions in the account. 
   class SnapTradeHoldingsTotalValue
     # Total value denominated in the currency of the `currency` field.
     attr_accessor :value
