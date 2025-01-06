733e154f2604a00ce73029124ae2783b75290c68
Passiv Ops <passiv-ops@passiv.com>
2025-01-06 17:45:52 +0000

    Regenerate SDKs

--- sdks/java/src/main/java/com/konfigthis/client/model/Status.java
+++ sdks/java/src/main/java/com/konfigthis/client/model/Status.java
@@ -79,7 +79,7 @@ public class Status {
    * @return version
   **/
   @javax.annotation.Nullable
-  @ApiModelProperty(example = "151", value = "")
+  @ApiModelProperty(example = "152", value = "")
 
   public Integer getVersion() {
     return version;
