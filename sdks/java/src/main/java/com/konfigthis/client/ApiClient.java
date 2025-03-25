ac3fbe06f3b212841c3d41f2bb7e6d8c84ae088c
Passiv Ops <passiv-ops@passiv.com>
2025-03-25 20:36:16 +0000

    Version Bump SDKs

--- sdks/java/src/main/java/com/konfigthis/client/ApiClient.java
+++ sdks/java/src/main/java/com/konfigthis/client/ApiClient.java
@@ -188,7 +188,7 @@ public class ApiClient extends ApiClientCustom {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("Konfig/5.0.83/java");
+        setUserAgent("Konfig/5.0.84/java");
 
         authentications = new HashMap<String, Authentication>();
     }
--- sdks/ruby/lib/snaptrade/version.rb
+++ sdks/ruby/lib/snaptrade/version.rb
@@ -8,5 +8,5 @@ Contact: api@snaptrade.com
 =end
 
 module SnapTrade
-  VERSION = '2.0.84'
+  VERSION = '2.0.85'
 end
