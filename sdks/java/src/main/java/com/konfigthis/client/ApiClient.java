c2cc2c728f6e9282c034587716b14a690d7c2026
Passiv Ops <passiv-ops@passiv.com>
2025-07-04 17:19:41 +0000

    Version Bump SDKs

--- sdks/java/src/main/java/com/konfigthis/client/ApiClient.java
+++ sdks/java/src/main/java/com/konfigthis/client/ApiClient.java
@@ -188,7 +188,7 @@ public class ApiClient extends ApiClientCustom {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("Konfig/5.0.109/java");
+        setUserAgent("Konfig/5.0.110/java");
 
         authentications = new HashMap<String, Authentication>();
     }
