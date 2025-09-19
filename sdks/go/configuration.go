1ae508287d433252a0a059bcb7733ea75a8e6f85
Passiv Ops <passiv-ops@passiv.com>
2025-09-19 15:58:55 +0000

    Version Bump SDKs

--- sdks/go/configuration.go
+++ sdks/go/configuration.go
@@ -105,7 +105,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "Konfig/1.0.107/go",
+		UserAgent:        "Konfig/1.0.108/go",
 		TokenUrl:   	  "",
 		Debug:            false,
         Context: context.Background(),
--- sdks/ruby/lib/snaptrade/version.rb
+++ sdks/ruby/lib/snaptrade/version.rb
@@ -8,5 +8,5 @@ Contact: api@snaptrade.com
 =end
 
 module SnapTrade
-  VERSION = '2.0.135'
+  VERSION = '2.0.136'
 end
