acddffaeac86fec409367ee1ed3a26b79ec9049b
Passiv Ops <passiv-ops@passiv.com>
2025-10-08 19:27:18 +0000

    Version Bump SDKs

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "snaptrade-python-sdk"
-VERSION = "11.0.139"
+VERSION = "11.0.140"
 # To install the library, run the following
 #
 # python setup.py install
--- sdks/python/snaptrade_client/__init__.py
+++ sdks/python/snaptrade_client/__init__.py
@@ -12,7 +12,7 @@
     Created by: https://snaptrade.com/
 """
 
-__version__ = "11.0.139"
+__version__ = "11.0.140"
 
 # import ApiClient
 from snaptrade_client.api_client import ApiClient
