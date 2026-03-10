aa9c097dbfbc1e03b25938df5faf8b9ae9230519
Passiv Ops <passiv-ops@passiv.com>
2026-03-10 21:57:35 +0000

    Version Bump SDKs

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "snaptrade-python-sdk"
-VERSION = "11.0.168"
+VERSION = "11.0.169"
 # To install the library, run the following
 #
 # python setup.py install
--- sdks/ruby/lib/snaptrade/version.rb
+++ sdks/ruby/lib/snaptrade/version.rb
@@ -8,5 +8,5 @@ Contact: api@snaptrade.com
 =end
 
 module SnapTrade
-  VERSION = '2.0.169'
+  VERSION = '2.0.170'
 end
