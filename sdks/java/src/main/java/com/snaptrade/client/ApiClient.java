b988126b6f8e5d8f9c25029ec625c518ae95b3f4
Passiv Ops <passiv-ops@passiv.com>
2026-03-30 13:06:13 +0000

    Version Bump SDKs

--- sdks/java/src/main/java/com/snaptrade/client/ApiClient.java
+++ sdks/java/src/main/java/com/snaptrade/client/ApiClient.java
@@ -188,7 +188,7 @@ public class ApiClient extends ApiClientCustom {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("Konfig/5.0.172/java");
+        setUserAgent("Konfig/5.0.173/java");
 
         authentications = new HashMap<String, Authentication>();
     }
--- sdks/python/snaptrade_client/__init__.py
+++ sdks/python/snaptrade_client/__init__.py
@@ -12,7 +12,7 @@
     Created by: https://snaptrade.com/
 """
 
-__version__ = "11.0.169"
+__version__ = "11.0.170"
 
 # import ApiClient
 from snaptrade_client.api_client import ApiClient
