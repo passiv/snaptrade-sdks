795d27892084c6b59233b0e87093ff5c2807ff90
Kirill Taletski <k-real@taletski.com>
2026-06-17 18:40:41 +0300

    remove redundant /api/v1 handling in Ruby SDK

--- sdks/ruby/lib/snaptrade/api_client_custom.rb
+++ sdks/ruby/lib/snaptrade/api_client_custom.rb
@@ -16,7 +16,7 @@ module SnapTrade
   class ApiClientCustom
     def self.request_hook(request, configuration)
       request.params[:timestamp] = Time.now.to_i
-      path = request.path[request.path.index('/api/v1/')..-1]
+      path = request.path
       query = Faraday::Utils.build_query(request.params)
       sig_object = {
         "content" => request.body.nil? || request.body.empty? ? nil : Hash[JSON.parse(request.body).sort],
