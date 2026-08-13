548200779985ad2de672160986c08baf4e3e0a12
Passiv Ops <passiv-ops@passiv.com>
2026-08-13 22:26:45 +0000

    Version Bump SDKs

--- sdks/go/configuration.go
+++ sdks/go/configuration.go
@@ -105,7 +105,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "Konfig/2.0.1/go",
+		UserAgent:        "Konfig/2.0.2/go",
 		TokenUrl:   	  "",
 		Debug:            false,
         Context: context.Background(),
