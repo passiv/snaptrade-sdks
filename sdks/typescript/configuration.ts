b9880d03081d2b9b328c3182432d5b4b70da4c59
Passiv Ops <passiv-ops@passiv.com>
2026-08-20 16:41:53 +0000

    Version Bump SDKs

--- sdks/typescript/configuration.ts
+++ sdks/typescript/configuration.ts
@@ -98,7 +98,7 @@ export class Configuration<TAuth extends AuthMode> {
         }
         this.basePath = param.basePath;
         this.baseOptions = param.baseOptions ?? {};
-        this.userAgent = param.userAgent === undefined ? "Konfig/12.1.5/typescript" : param.userAgent;
+        this.userAgent = param.userAgent === undefined ? "Konfig/12.1.6/typescript" : param.userAgent;
         this.formDataCtor = param.formDataCtor;
     }
 
