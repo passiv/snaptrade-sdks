3afebbc673f96bf7c6ea044582a87793042930d9
Passiv Ops <passiv-ops@passiv.com>
2026-09-03 18:10:06 +0000

    Version Bump SDKs

--- sdks/csharp/src/SnapTrade.Net/Client/Configuration.cs
+++ sdks/csharp/src/SnapTrade.Net/Client/Configuration.cs
@@ -33,7 +33,7 @@ namespace SnapTrade.Net.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "6.0.13";
+        public const string Version = "6.0.14";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -116,7 +116,7 @@ namespace SnapTrade.Net.Client
         public Configuration()
         {
             Proxy = null;
-            UserAgent = "Konfig/6.0.13/csharp";
+            UserAgent = "Konfig/6.0.14/csharp";
             BasePath = "https://api.snaptrade.com";
             DefaultHeaders = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
@@ -522,7 +522,7 @@ namespace SnapTrade.Net.Client
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
             report += "    Version of the API: 1.0.0\n";
-            report += "    SDK Package Version: 6.0.13\n";
+            report += "    SDK Package Version: 6.0.14\n";
 
             return report;
         }
--- sdks/ruby/lib/snaptrade/version.rb
+++ sdks/ruby/lib/snaptrade/version.rb
@@ -8,5 +8,5 @@ Contact: api@snaptrade.com
 =end
 
 module SnapTrade
-  VERSION = '3.0.13'
+  VERSION = '3.0.14'
 end
